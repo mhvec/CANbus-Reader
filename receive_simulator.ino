4a810f3dd56424d7ec8b6e4dc291fefa52c3022b
Magnus <79435486+mhvec@users.noreply.github.com>
2022-09-15 12:49:08 -0400

    Update receive_simulator.ino

--- receive_simulator.ino
+++ receive_simulator.ino
@@ -1,6 +1,6 @@
 int rpm = 950;
 int ect = 68;
-int lambda = 0;
+int lambda = 1337;
 int mappress = 100;
 int wait = 20;
 
@@ -10,34 +10,33 @@ Serial.begin(115200);
 
 void loop() {
   // put your main code here, to run repeatedly:
-   Serial.print("ECT: ");
-   Serial.print(ect);
-   Serial.print("    ");
-   Serial.print("Lambda: ");
-   Serial.print(lambda);
-   Serial.print("    ");
-   Serial.print("MAP: ");
-   Serial.print(mappress);
-   Serial.print("    ");
-   Serial.print("RPM: ");
    Serial.print(rpm);
+   Serial.print("\t");
+   
+   Serial.print(ect);
+   Serial.print("\t");
+   
+   Serial.print(lambda);
+   Serial.print("\t");
+   
+   Serial.print(mappress);
    Serial.println("");
 
-   rpm+=5;
+   rpm+=100;
    ect+=1;
-   lambda+=1;
+   lambda+=777;
    mappress+=1;
 
-   if (rpm > 8200)
+   if (rpm > 10000)
    rpm = 950;
 
    if (ect > 75)
    ect = 68;
 
-   if (lambda > 9)
-   lambda = 0;
+   if (lambda > 30000)
+   lambda = 1337;
 
    if (mappress > 200)
-   mappress = 100;
+   mappress = 60;
    delay(wait);
 }
